23e2df2686979e1929a1258a057c2ee20e2da70e
avgoncharov <goncharov.andrey.v@gmail.com>
2016-06-06 17:17:01 +0300

    Добавил гулп и конфигурацию system.js.

---
+++ FrontEndResearch/gulpfile.js
@@ -0,0 +1,29 @@
+﻿var gulp = require('gulp');
+var typescript = require('gulp-tsc');
+var webpack = require('webpack-stream');
+var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
+var angularProtractor = require('gulp-angular-protractor');
+
+var vendorLibsPath = './libs';
+
+gulp.task('move_angularlibs', function () {
+	return gulp.src([
+		'node_modules/@angular/**/*.umd.js',
+		'node_modules/@angular/**/testing.js',
+	]).pipe(gulp.dest(vendorLibsPath + '/@angular'));
+});
+
+gulp.task('move_libs', function () {
+	return gulp.src([
+	  'node_modules/angular2-cookie/bundles/angular2-cookie.js',
+	  'node_modules/angular2-cookie/bundles/angular2-cookie.js.map',
+	  'node_modules/reflect-metadata/Reflect.js',
+	  'node_modules/es6-shim/es6-shim.min.js*',
+	  'node_modules/systemjs/dist/*.*',
+	  'node_modules/jquery/dist/jquery.*js',
+	  'node_modules/bootstrap/dist/js/bootstrap*.js',
+	  'node_modules/rxjs/bundles/Rx.js',
+	  'node_modules/zone.js/dist/zone.js'
+	]).pipe(gulp.dest(vendorLibsPath));
+});
---
+++ FrontEndResearch/systemjs.config.js
@@ -0,0 +1,83 @@
+﻿(function (global) {
+
+	// map tells the System loader where to look for things
+	var map = {
+		'@angular': 'libs/@angular',
+		'angular2-cookie/core': 'libs',
+	};
+
+	// packages tells the System loader how to load when no filename and/or no extension
+	var packages = {
+		AppScripts: {
+			main: 'main.js',
+			defaultExtension: 'js'
+		},
+		components: {
+			main: 'main.js',
+			defaultExtension: 'js'
+		},
+		services: {
+			main: 'main.js',
+			defaultExtension: 'js'
+		},
+		utils: {
+			main: 'main.js',
+			defaultExtension: 'js'
+		},
+		//'app/utils/http-client': {
+		//	main: 'index.js',
+		//},
+		//'app/services/auth': {
+		//	main: 'index.js',
+		//},
+		//'app/services/search': {
+		//	main: 'index.js',
+		//},
+		//'app/components/base': {
+		//	main: 'index.js'
+		//},
+		//'app/components/search': {
+		//	main: 'index.js'
+		//},
+		//'app/components/promo': {
+		//	main: 'index.js'
+		//},
+		//'angular2-cookie/core': {
+		//	main: 'angular2-cookie.js'
+		//},
+	};
+
+	var angularPackageNames = [
+		'common',
+		'compiler',
+		'core',
+		'http',
+		'platform-browser',
+		'platform-browser-dynamic',
+		'router',
+		'router-deprecated',
+		'testing',
+		'upgrade',
+	];
+
+	// add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+	angularPackageNames.forEach(function (pkgName) {
+		packages['@angular/' + pkgName] = {
+			main: pkgName + '.umd.js',
+			defaultExtension: 'js'
+		};
+	});
+
+	var config = {
+		map: map,
+		packages: packages
+	}
+
+	// filterSystemConfig - index.html's chance to modify config before we register it.
+	if (global.filterSystemConfig) {
+		global.filterSystemConfig(config);
+	}
+
+	System.config(config);
+
+})(this);
