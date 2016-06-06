08801ca1024ae1df2ec0258c8eb96f8ec0d06f85
avgoncharov <goncharov.andrey.v@gmail.com>
2016-06-06 17:24:05 +0300

    Добавил тестирование get/set.

---
+++ FrontEndResearch/scripts/Data.ts
@@ -0,0 +1,15 @@
+﻿//Изучаю как работать с get/set.
+export class Data
+{
+	private someDataVal: string;
+
+	get someData() : string{
+		console.log("Get SomeData.");
+		return this.someDataVal;
+	}
+
+	set someData(value: string) {
+		console.log("Set SomeData.");
+		this.someDataVal = value;
+	}
+}
--- FrontEndResearch/scripts/app.ts
+++ FrontEndResearch/scripts/app.ts
@@ -1,13 +1,17 @@
 ﻿import { Component } from '@angular/core';
+import {Data} from "./Data"
 
 @Component({
 	selector: 'app',
-	template: '<h1>APP</h1>'
+	template: '<h1>{{data.someData}}</h1>'
 })
 
 export class AppComponent{
 	hello: string;
+	data: Data;
 
 	constructor() {
+		this.data = new Data();
+		this.data.someData = "! xyz !";
 	}
 }
