a3d8779c9b55110d4f0f784ab823c5b066555ea9
avgoncharov <goncharov.andrey.v@gmail.com>
2016-06-06 17:03:27 +0300

    Добавил ангуляр.

---
+++ FrontEndResearch/scripts/app.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app',
+	template: '<h1>APP</h1>'
+})
+
+export class AppComponent{
+	hello: string;
+
+	constructor() {
+	}
+}
---
+++ FrontEndResearch/scripts/main.ts
@@ -0,0 +1,10 @@
+﻿///<reference path="../typings/main.d.ts" />
+
+import { provide } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { LocationStrategy, HashLocationStrategy } from "@angular/common"
+import { ROUTER_PROVIDERS} from '@angular/router';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app'
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS,HTTP_PROVIDERS]);
