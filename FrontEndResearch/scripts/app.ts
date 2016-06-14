0dab52593438a500e8f750e31058f423d6fa11d4
avgoncharov <goncharov.andrey.v@gmail.com>
2016-06-14 15:14:25 +0300

    Добавил ТС с локализацией.

--- FrontEndResearch/scripts/app.ts
+++ FrontEndResearch/scripts/app.ts
@@ -1,17 +1,22 @@
 ﻿import { Component } from '@angular/core';
 import {Data} from "./Data"
+import {Strings, LocaleTypes, CurrentLocale}  from "./Localization"
 
 @Component({
 	selector: 'app',
-	template: '<h1>{{data.someData}}</h1>'
+	template: '<div>' +
+	'<div><input name="xyz" type="radio" (click)="currLocal.setLocale(localType.RuRu)" value="RuRu"/>RuRu</div>' +
+		'<div><input name="xyz" type="radio" (click)="currLocal.setLocale(localType.EnUs)" value="EnUs"/>EnUs</div>' +
+	'<h1>{{lc.test}}</h1>' +
+	'<h1>{{lc.test2.default}}</h1>' +
+	'<h1>{{lc.test2.text.default}}</h1>' +
+	'<h1>{{lc.test2.text.innerText}}</h1>' +
+		'</div>'
 })
 
 export class AppComponent{
 	hello: string;
-	data: Data;
-
-	constructor() {
-		this.data = new Data();
-		this.data.someData = "! xyz !";
-	}
+	localType = LocaleTypes;
+	currLocal = CurrentLocale;
+	lc = Strings;
 }
